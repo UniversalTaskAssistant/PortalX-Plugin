87905837efdc3a08be9b00f41d62c91907cd393c
MulongXie <dsh15325@163.com>
2024-11-26 14:18:14 +1100

    rename folder

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from UTAWeb import UTAWeb
+from B import UTAWeb
 import urllib.parse
 from multiprocessing import Process
 import os
