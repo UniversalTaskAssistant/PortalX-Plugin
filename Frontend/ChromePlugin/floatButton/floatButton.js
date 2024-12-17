c9e5159aea90a8b1b3cf4cea7e6a3d97d345e8e5
MulongXie <dsh15325@163.com>
2024-12-18 06:49:34 +1100

    show website logo while hovering

--- Frontend/ChromePlugin/floatButton/floatButton.js
+++ Frontend/ChromePlugin/floatButton/floatButton.js
@@ -13,11 +13,36 @@ function initializeFloatButton() {
             .appendTo('body');
 
         // Add logo image
-        $('<img>')
+        const $defaultLogo = $('<img>')
             .attr('src', chrome.runtime.getURL('../img/logo2.png'))
+            .addClass('default-logo')
             .appendTo($floatButton);
 
-        console.log("Float button added to DOM");
+        // Add favicon image (initially hidden)
+        const $favicon = $('<img>')
+            .addClass('favicon-logo')
+            .hide()
+            .appendTo($floatButton);
+
+        // Get favicon from the current page
+        const currentFavicon = $('link[rel="icon"]').attr('href') || 
+                             $('link[rel="shortcut icon"]').attr('href') ||
+                             window.location.origin + '/favicon.ico';
+        
+        if (currentFavicon) {
+            $favicon.attr('src', currentFavicon);
+        }
+
+        // Add hover effects
+        $floatButton
+            .on('mouseenter', () => {
+                $defaultLogo.hide();
+                $favicon.show();
+            })
+            .on('mouseleave', () => {
+                $favicon.hide();
+                $defaultLogo.show();
+            });
 
         // Create popup
         const $popup = $('<iframe>')
@@ -32,8 +57,12 @@ function initializeFloatButton() {
             event.stopPropagation();
             if ($popup.is(':hidden')) {
                 $popup.show();
+                $defaultLogo.hide();
+                $favicon.show();
             } else {
                 $popup.hide();
+                $favicon.hide();
+                $defaultLogo.show();
             }
         });
 
@@ -41,6 +70,8 @@ function initializeFloatButton() {
         $(document).on('click', (event) => {
             if (!$(event.target).closest('.popup, .float-button').length) {
                 $popup.hide();
+                $favicon.hide();
+                $defaultLogo.show();
             }
         });
 
@@ -48,6 +79,8 @@ function initializeFloatButton() {
         window.addEventListener('message', (event) => {
             if (event.data === 'minimize') {
                 $popup.hide();
+                $favicon.hide();
+                $defaultLogo.show();
             }
         });
 
